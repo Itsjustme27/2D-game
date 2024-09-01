80ea237c830f69f3c3393984a139bd3006311577
Itsjustme27 <reachprayush@gmail.com>
2024-09-01 23:30:19 +0545

    added keyListener

--- 2D-game/src/GamePanel.java
+++ 2D-game/src/GamePanel.java
@@ -19,11 +19,18 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    KeyHandler keyH = new KeyHandler();
+
     Thread gameThread;
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));  // sets the size of this class(JPanel)
-
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); //if set to true, all the drawing from this component will be done in a offscreen painting buffer. (added to improve rendering performance)
     }
@@ -40,25 +47,32 @@ public class GamePanel extends JPanel implements Runnable{
         // 2) DRAW : draw the screen with updated information
 
         while(gameThread != null) {
+            long currentTime = System.nanoTime();
+            System.out.println("Current Time" + currentTime);
             update();
             repaint();
         }
     }
 
     public void update() {
-
+        if(keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        }else if(keyH.downPressed == true) {
+            playerY += playerSpeed;
+        }else if(keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        }else if(keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
     }
 
-    public void painComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D)g;
 
         g2.setColor(Color.white);
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY,  tileSize, tileSize);  
         g2.dispose();
     }  
-    // TODO: Not displaying, make it display
-
-     
 }
